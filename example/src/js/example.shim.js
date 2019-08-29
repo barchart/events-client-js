62fe7b4c2406a4d37fa8461480244c930cf19c7c
Bryan Ingle <bryan.ingle@barchart.com>
2019-08-29 12:04:06 -0500

    Fix

--- example/src/js/example.shim.js
+++ example/src/js/example.shim.js
@@ -5,6 +5,10 @@ const CustomerType = require('@barchart/events-api-common/lib/data/CustomerType'
 	EventType = require('@barchart/events-api-common/lib/data/EventType'),
 	ProductType = require('@barchart/events-api-common/lib/data/ProductType');
 
+const EventBatcher = require('./../../../lib/engine/EventBatcher'),
+	EventGateway = require('./../../../lib/gateway/EventGateway'),
+	ReportGateway = require('./../../../lib/gateway/ReportGateway');
+
 const packageJSON = require('../../../package');
 
 module.exports = (() => {
@@ -17,6 +21,11 @@ module.exports = (() => {
 	window.Barchart.EventJobStatus = EventJobStatus;
 	window.Barchart.EventType = EventType;
 	window.Barchart.ProductType = ProductType;
+
+	window.Barchart.Event = { };
+	window.Barchart.Event.EventBatcher = EventBatcher;
+	window.Barchart.Event.EventGateway = EventGateway;
+	window.Barchart.Event.ReportGateway = ReportGateway;
 
 	window.Barchart.ClientVersion = packageJSON.version;
 })();
